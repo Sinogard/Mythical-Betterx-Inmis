8d16b6fc2b2ee74e8b3876b77fd664e264fe7c5e
Sinogard <88293626+Sinogard@users.noreply.github.com>
2024-10-09 17:38:03 -0700

    Update InmisClothConfig.java
    added new backpacks

--- src/main/java/draylar/inmis/config/InmisClothConfig.java
+++ src/main/java/draylar/inmis/config/InmisClothConfig.java
@@ -12,16 +12,34 @@ import java.util.List;
 @Config(name = "inmis")
 @Config.Gui.Background("minecraft:textures/block/stone.png")
 public class InmisClothConfig implements ConfigData {
-
     public List<BackpackInfo> backpacks = Arrays.asList(
-            BackpackInfo.of("baby", 3, 1, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
-            BackpackInfo.of("frayed", 9, 1, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, true),
-            BackpackInfo.of("plated", 9, 2, false, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            BackpackInfo.of("gilded", 9, 3, false, SoundEvents.ITEM_ARMOR_EQUIP_GOLD),
-            BackpackInfo.of("bejeweled", 9, 5, false, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND),
-            BackpackInfo.of("blazing", 9, 6, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
-            BackpackInfo.of("withered", 11, 6, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
-            BackpackInfo.of("endless", 15, 6, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER)
+            BackpackInfo.of("baby", 9, 1, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("frayed", 9, 3, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, true),
+            BackpackInfo.of("copper", 9, 4, false, SoundEvents.ITEM_ARMOR_EQUIP_GOLD),
+            BackpackInfo.of("plated", 9, 5, false, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
+            BackpackInfo.of("bronze", 9, 6, false, SoundEvents.ITEM_ARMOR_EQUIP_GOLD),
+            BackpackInfo.of("cincinnacite", 10, 6, false, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND),
+            BackpackInfo.of("gilded", 10, 7, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("osmium", 11, 7, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("steel", 12, 7, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("durasteel", 13, 7, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("kyber", 13, 8, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("banglum", 14, 8, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("terminite", 15, 8, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("tidesinger", 15, 8, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("runite", 16, 8, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("promethium", 17, 8, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("bejeweled", 18, 8, false, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("blazing", 18, 9, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("adamantite", 19, 9, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("paladium", 20, 9, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("mythril", 21, 9, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("orichalcum", 22, 9, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("hallowed", 22, 10, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("withered", 22, 11, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("aeternium", 23, 12, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("metallurgium", 23, 13, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER),
+            BackpackInfo.of("endless", 23, 11, true, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER)
     );
 
     public boolean unstackablesOnly = false;
